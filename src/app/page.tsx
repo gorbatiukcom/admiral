666a8fa707820cef1654739ca1c7a6f3b8a0cd5b
Oleg Gorbatiuk <newgorbole@gmail.com>
2024-04-07 21:08:06 +0200

    Fix home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,77 +1,9 @@
-"use client";
+import { Metadata } from "next";
 
-import { Box, Button, Flex, Heading } from "@chakra-ui/react";
+import { Home } from "./home";
 
-import { Link } from "@/components/link";
-import { SocialMediaLinks } from "@/components/SocialMediaLinks";
+export const metadata: Metadata = {
+  title: "Admiral Interior",
+};
 
-export default function Home() {
-  return (
-    <Box
-      bgColor="red"
-      bg="linear-gradient(rgba(26,26,26,0.25), rgba(26,26,26,0.25)), url(/images/bg-image.png)"
-      bgRepeat="no-repeat"
-      bgPosition={["25% 75%", "center, center"]}
-      bgSize="cover"
-      height="var(--chakra-vh)"
-    >
-      <Flex
-        flexDirection="column"
-        justifyContent="flex-start"
-        height="100%"
-        px={["16px", "80px"]}
-        pb={[4, 10]}
-      >
-        <Flex minHeight={["152px", "186px"]}></Flex>
-        <Flex flexDirection="column" height="100%" justifyContent="center">
-          <Heading
-            fontSize={["26px", "60px"]}
-            fontWeight={400}
-            color="textPrimaryWhite"
-            maxWidth={["240px", "600px"]}
-          >
-            Nowoczesne podejście do aranżacji wnętrz
-          </Heading>
-          <Link href="/services/order" mt="120px" width="max-content">
-            <Button
-              width="max-content"
-              height={["48px", "60px"]}
-              px={["40px", "70px"]}
-              background="black"
-              color="textPrimaryWhite"
-              fontSize={["20px", "24px"]}
-              borderRadius={8}
-              transition="0.2s"
-              fontWeight={400}
-              _hover={{
-                background: "brand.blue",
-              }}
-            >
-              Wycena Online
-            </Button>
-          </Link>
-        </Flex>
-        <Flex width="100%" justifyContent="flex-end" gap={[4, 6]}>
-          {SocialMediaLinks.map(({ url, name }) => (
-            <Link
-              key={name}
-              href={url}
-              color="textPrimaryWhite"
-              fontSize={["14px", "18px"]}
-              lineHeight={["21px", "40px"]}
-              cursor="pointer"
-              fontWeight={600}
-              _hover={{
-                color: "brand.blue",
-              }}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {name}
-            </Link>
-          ))}
-        </Flex>
-      </Flex>
-    </Box>
-  );
-}
+export default Home;
